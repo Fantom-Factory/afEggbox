06407563b72f05d27817f3d203099e2e458c2b45
Steve Eynon <steve.eynon@alienfactory.co.uk>
2016-04-11 13:36:00 +0100

    .

--- compile-less.cmd
+++ compile-less.cmd
@@ -3,8 +3,7 @@
 cd C:\Projects\Fantom-Factory\Eggbox
 
 @echo Compiling website.less
-call fan afLess4f -x etc\less\website.less etc\web-static\css\website.min.css
-rem call fpm run afLess4f -x etc\less\website.less etc\web-static\css\website.min.css
+call fpm run afLess4f -x etc\less\website.less etc\web-static\css\website.min.css
 
 @echo Done.
 @rem pause
